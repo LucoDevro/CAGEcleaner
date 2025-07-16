f02d5d4b842441a7cbda919f06e35e7f8e93b33c
Lucas De Vrieze <lucas.devrieze@kuleuven.be>
2025-07-16 12:55:30 +0200

    Add optional verbosity

--- src/cagecleaner/cagecleaner.py
+++ src/cagecleaner/cagecleaner.py
@@ -57,7 +57,7 @@ DEREPLICATE_SCRIPT = os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])),
 global TEMP_DIR
 global GENOMES
 global SKDER_OUT
-
+global VERBOSE
 
 def parse_arguments():
     """
@@ -103,6 +103,7 @@ def parse_arguments():
     args_general.add_argument('-c', '--cores', dest = 'cores', default = 1, type = int, help = "Number of cores to use (default: 1)")    
     args_general.add_argument('-h', '--help', action = 'help', help = "Show this help message and exit")      
     args_general.add_argument('-v', '--version', action = "version", version = "%(prog)s " + __version__)
+    args_general.add_argument('--verbose', dest = 'verbose', default = False, action = 'store_true', help = "Enable verbose logging")
     
     args_io = parser.add_argument_group('Input / Output')
     args_io.add_argument('-s', '--session', dest = "session_file", type = check_exists, help = "Path to cblaster session file")
@@ -142,6 +143,9 @@ def load_session(path_to_session: str) -> Session:
     
     session = Session.from_file(path_to_session)
     
+    if VERBOSE:
+        print("Session loaded!")
+    
     return session
 
 
@@ -155,14 +159,21 @@ def get_scaffolds(session: Session, excluded: list = []) -> list:
     print("\n--- STEP 1: Extracting scaffold IDs. ---")
     
     # Make a temporary copy of the binary table
+    if VERBOSE:
+        print("Generating binary table from session")
     with open(_temp("binary.txt"), "w") as handle:
         session.format("binary", fp = handle)
 
     # Read the file using a variable series of spaces as separator and extract the second column (containing the scaffold IDs)
+    if VERBOSE:
+        print("Reading binary table")
     scaffolds = pd.read_table(_temp("binary.txt"), sep = "\\s{2,}", engine = 'python', usecols = [1])['Scaffold'].to_list()
     
     # Exclude requested scaffolds
-    scaffolds = list(set(scaffolds) - set(excluded))
+    if excluded != ['']:
+        if VERBOSE:
+            print(f"Excluding scaffolds {', '.join(excluded)}")
+        scaffolds = list(set(scaffolds) - set(excluded))
 
     print(f"Extracted {len(scaffolds)} scaffold IDs")
     
@@ -181,21 +192,30 @@ def get_assemblies(scaffolds: list, excluded: list = []) -> list:
     print("\n--- STEP 2: Retrieving genome assembly IDs. ---")
     
     # save the scaffold list in a temporary file
+    if VERBOSE:
+        print("Writing list of scaffolds")
     with open(_temp('scaffolds.txt'), 'w') as handle:
         handle.writelines('\n'.join(scaffolds))
     
     # map to assembly IDs using E-utilities
+    if VERBOSE:
+        print("Calling NCBI Entrez Utilities")
     home = os.getcwd()
     os.chdir(ASSIGNED_TEMP_DIR)
     subprocess.run(['bash', ACCESSIONS_SCRIPT, 'scaffolds.txt'], check = True)
     os.chdir(home)
     
     # read the result file
+    if VERBOSE:
+        print("Reading results")
     with open(_temp('assembly_accessions'), 'r') as handle:
         assemblies = [l.rstrip() for l in handle.readlines()]
         
     # Excluded requested assemblies
-    assemblies = list(set(assemblies) - set(excluded))
+    if excluded != ['']:
+        if VERBOSE:
+            print(f"Excluding assemblies {', '.join(excluded)}")
+        assemblies = list(set(assemblies) - set(excluded))
     
     return assemblies
 
@@ -217,12 +237,16 @@ def download_genomes(assemblies: list, batch_size: int = 300) -> None:
     versionless_assemblies = [acc.split('.')[0] for acc in assemblies]
     
     # Prepare the batches and save them in a temporary file
+    if VERBOSE:
+        print('Preparing download batches')
     with open(_temp('download_batches.txt'), 'w') as file:
         download_batches = list(batched(versionless_assemblies, batch_size))
         for batch in download_batches:
             file.write(' '.join(batch) + '\n')
 
     # Run the bash script to download genomes:
+    if VERBOSE:
+        print('Calling NCBI Datasets CLI')
     home = os.getcwd()
     os.chdir(ASSIGNED_TEMP_DIR)
     subprocess.run(["bash", DOWNLOAD_SCRIPT], check=True)
@@ -261,6 +285,8 @@ def map_scaffolds_to_assemblies(scaffolds: list, assemblies: list) -> dict:
     mappings = {}
     
     for assmbl in assemblies:
+        if VERBOSE:
+            print(f"Mapping {assmbl}")
         
         # Find the path to the downloaded fasta file for this assembly ID, ignoring version digits
         try:
@@ -285,6 +311,9 @@ def map_scaffolds_to_assemblies(scaffolds: list, assemblies: list) -> dict:
                 # map the deprefixed IDs back to the original ones
                 found_scaffolds = [map_back(s, scaffolds) for s in found_scaffolds_no_prefix]
                 
+                if VERBOSE:
+                    print(f"Found scaffolds {', '.join(found_scaffolds)}")
+                
                 # add a mapping item for all scaffolds with a cblaster hit
                 for scaff in found_scaffolds:
                     mappings[scaff] = assmbl
@@ -293,6 +322,8 @@ def map_scaffolds_to_assemblies(scaffolds: list, assemblies: list) -> dict:
                 print(f'No corresponding scaffold accession ID could be found for {assmbl}!')
     
     # Write scaffold-assembly pairs to report file
+    if VERBOSE:
+        print("Writing mapping pairs to file")
     with open('scaffold_assembly_pairs.txt', 'w') as handle:
         handle.write('\n'.join([s + '\t' + a for s,a in mappings.items()]))
     
@@ -314,6 +345,8 @@ def dereplicate_genomes(ani_threshold: float = 99.0, nb_cores: int = 1) -> None:
     """
     print("\n--- STEP 5: Dereplicating genomes. ---")
     
+    if VERBOSE:
+        print("Calling skDER")
     home = os.getcwd()
     os.chdir(ASSIGNED_TEMP_DIR)
     subprocess.run(['bash', DEREPLICATE_SCRIPT, str(ani_threshold), str(nb_cores)], check = True)
@@ -351,6 +384,8 @@ def parse_dereplication_clusters(scaffold_assembly_pairs: dict) -> pd.DataFrame:
     def map_scaffolds(assembly: str, scaffold_assembly_pairs: dict) -> list:
         return [s for s,a in scaffold_assembly_pairs.items() if a == assembly]
     
+    if VERBOSE:
+        print("Reading skDER table")
     # Parse the skDER output table. Rename the columns and extract Assembly IDs on-the-fly.
     genome_clusters_df = pd.read_table(os.path.join(SKDER_OUT, "skDER_Clustering.txt"), 
                                        converters={'assembly': extract_assembly,
@@ -367,11 +402,15 @@ def parse_dereplication_clusters(scaffold_assembly_pairs: dict) -> pd.DataFrame:
     genome_clusters_records = genome_clusters_df.to_dict(orient = 'index')
     
     # Build a similar dictionary using Nucleotide accessions as keys, replicating representative assemblies if needed
+    if VERBOSE:
+        print("Building scaffold-representative assembly table")
     genome_clusters = {}
-    for assembly, representative in genome_clusters_records.items():
+    for assembly, representative_status in genome_clusters_records.items():
         mapped_scaffolds = map_scaffolds(assembly, scaffold_assembly_pairs) # map assembly to a list of scaffold(s) with a cblaster hit
         for scaffold in mapped_scaffolds:
-            genome_clusters[scaffold] = representative
+            if VERBOSE:
+                print(f'Mapping scaffold {scaffold} from assembly {assembly} to representative {representative_status["representative"]}')
+            genome_clusters[scaffold] = representative_status
     
     # Construct a new dataframe from this Nucleotide-keyed dictionary
     genome_clusters = pd.DataFrame.from_dict(genome_clusters, orient = 'index', columns = ['representative', 'dereplication_status'])
@@ -423,6 +462,8 @@ def recover_hits(session: Session, genome_clusters_mapping: pd.DataFrame, not_by
     if not(not_by_content):
         
         # Read the relevant columns from a temporary copy of the cblaster binary table
+        if VERBOSE:
+            print("Generating binary table from session file")
         with open(_temp("binary_recovery.txt"), "w") as handle:
             session.format("binary", fp = handle)
         hits = pd.read_table(_temp("binary_recovery.txt"), sep = "\\s{2,}", engine = 'python', 
@@ -431,6 +472,8 @@ def recover_hits(session: Session, genome_clusters_mapping: pd.DataFrame, not_by
         
         ## Hits are recovered within dereplication clusters so we will check the hits of each dereplication cluster
         # Get the IDs of all assemblies grouped by their dereplication representative
+        if VERBOSE:
+            print("Grouping hits by representative assembly")
         grouped_mapping = genome_clusters_mapping.reset_index(names = 'scaffold').groupby('representative').agg(list)
         genome_groups = dict(zip(grouped_mapping.index, grouped_mapping['scaffold']))
         
@@ -439,31 +482,43 @@ def recover_hits(session: Session, genome_clusters_mapping: pd.DataFrame, not_by
             # Get the cblaster hit data for scaffolds of this dereplication cluster
             hits_this_group = hits.loc[group]
             
+            if VERBOSE:
+                print(f"Grouping hits for representative {representative} by content")
             # Split the dereplication grouping further into gene cluster subgroups as reflected by the number of homologs of each query gene
             hits_this_group_by_content_group = [l.to_list() for l in hits_this_group.groupby(
                                                 list(set(hits_this_group.columns).difference({'Score'})) # to get all query gene columns
                                                 ).groups.values()]
             
             # Loop over all structural subgroups
+            if VERBOSE:
+                print("Checking all structural subgroups")
             for content_group in hits_this_group_by_content_group:
                 scores_this_content_group = hits_this_group.loc[content_group, 'Score'] # cblaster scores
                 mode_score_this_content_group = float(scores_this_content_group.mode().iloc[0]) # modal cblaster score
                 zscores_this_content_group = scores_this_content_group.apply(zscore, by_row = False) # zscores
                 
                 # If the result of the z-score calculation yields all NaNs, all cblaster scores were identical, implying there is no score outlier.
-                # In that case, we can continue flagging different gene cluster contents, if any
+                no_outlier_screening_needed = zscores_this_content_group.isna().all()
+                if VERBOSE and no_outlier_screening_needed:
+                    print("No outliers in this subgroup!")
+                
+                # In case there are not score outliers, we can continue flagging different gene cluster contents, if any
                 # If the user is not interested in flagging score outlier hits, we end up in this case anyway.
-                if not_by_score or zscores_this_content_group.isna().all():
+                if not_by_score or no_outlier_screening_needed:
                     
                     # If the dereplication representative is one of the assemblies that has a scaffold in this structural subgroup, 
                     # then we already have a hit from this subgroup, so we can skip this one.
                     dereplication_status_this_content_group = [genome_clusters_mapping.loc[m, 'dereplication_status'] for m in content_group]
                     if "dereplication_representative" in dereplication_status_this_content_group:
+                        if VERBOSE:
+                            print(f"Skipped representative group {representative}")
                         continue
                     
                     # If the dereplication representative is not in this structural subgroup, randomly pick a member as a representative hit to keep
                     else:
                         content_group_representative = choice(content_group)
+                        if VERBOSE:
+                            print(f"Picked {content_group_representative} as representative for content subgroup")
                         updated_mapping.at[content_group_representative, 'dereplication_status'] = 'readded_by_cluster_content'
                         recovered += 1
                 
@@ -473,17 +528,25 @@ def recover_hits(session: Session, genome_clusters_mapping: pd.DataFrame, not_by
                     # Select hits that have an absolute z-score above the threshold,
                     # and of which the cblaster score is sufficiently different from the modal score,
                     # to avoid 'false' outliers that are just a little bit more different than most hits
+                    if VERBOSE:
+                        print(f'Calculating z scores for representative group {representative}')
                     outliers_this_content_group = zscores_this_content_group.loc[
                         (zscores_this_content_group.abs() >= outlier_z) &
                         ((scores_this_content_group - mode_score_this_content_group).abs() >= min_score_diff)].index
                     
                     # All score outliers will be added
+                    if VERBOSE:
+                        print("Checking all outliers")
                     for outlier in outliers_this_content_group.to_list():
                         # If the overarching assembly of this outlier is the dereplication representative, skip flagging it
                         dereplication_status_outlier = genome_clusters_mapping.loc[outlier, 'dereplication_status']
                         if dereplication_status_outlier == "dereplication_representative":
+                            if VERBOSE:
+                                print(f"Skipped representative {representative}")
                             continue
                         else:
+                            if VERBOSE:
+                                print(f'Recovered {outlier} as score outlier')
                             updated_mapping.at[outlier, 'dereplication_status'] = 'readded_by_outlier_score'
                             recovered += 1
                             
@@ -494,11 +557,15 @@ def recover_hits(session: Session, genome_clusters_mapping: pd.DataFrame, not_by
                     dereplication_status_non_outliers_this_content_group = [genome_clusters_mapping.loc[m, 'dereplication_status'] 
                                                                               for m in non_outliers_this_content_group]
                     if "dereplication_representative" in dereplication_status_non_outliers_this_content_group:
+                        if VERBOSE:
+                            print(f"Skipped representative group {representative}")
                         continue
                     
                     # If not, pick a random representative from the non-outlier hits
                     else:
                         content_group_representative = choice(non_outliers_this_content_group)
+                        if VERBOSE:
+                            print(f"Picked {content_group_representative} as representative for content subgroup")
                         updated_mapping.at[content_group_representative, 'dereplication_status'] = 'readded_by_cluster_content'
                         recovered += 1
     
@@ -510,6 +577,8 @@ def recover_hits(session: Session, genome_clusters_mapping: pd.DataFrame, not_by
     updated_mapping = updated_mapping.sort_values(by = ['representative', 'dereplication_status'])
     
     # Write report file
+    if VERBOSE:
+        print("Writing dereplication status table")
     updated_mapping.reset_index(names = 'scaffold').to_csv('genome_cluster_status.txt', sep = "\t", index = False)
     
     print(f"Recovered {recovered} scaffolds")
@@ -548,11 +617,15 @@ def generate_output(dereplicated_scaffolds:list, session:Session) -> None:
     session_dict = session.to_dict()
     
     # Make a deep copy to start carving out the new session
+    if VERBOSE:
+        print("Parsing original session file")
     filtered_session_dict = deepcopy(session_dict)
     
     ## Filtering the json session file
     # Remove all cluster hits that do not link with a dereplicated scaffold
     # Remove strains that have no hits left
+    if VERBOSE:
+        print("Filtering session file")
     for org_idx, org in reversed(list(enumerate(session_dict['organisms']))):
         for hit_idx, hit in reversed(list(enumerate(org['scaffolds']))):
             if hit['accession'] not in dereplicated_scaffolds:
@@ -561,22 +634,32 @@ def generate_output(dereplicated_scaffolds:list, session:Session) -> None:
                     filtered_session_dict['organisms'].pop(org_idx)
                 
     # Create the filtered cblaster session object
+    if VERBOSE:
+        print("Creating filtered cblaster session")
     filtered_session = Session.from_dict(filtered_session_dict)
     
     # Generate the outputs
     # session file
+    if VERBOSE:
+        print("Writing filtered session file")
     with open("filtered_session.json", "w") as session_handle:
         filtered_session.to_json(fp = session_handle)
         
     # binary table
+    if VERBOSE:
+        print("Writing filtered binary table")
     with open("filtered_binary.txt", 'w') as filtered_binary_handle:
         filtered_session.format(form = "binary", fp = filtered_binary_handle)
         
     # summary file
+    if VERBOSE:
+        print('Writing filtered summary file')
     with open("filtered_summary.txt", "w") as filtered_summary_handle:
         filtered_session.format(form = "summary", fp = filtered_summary_handle)    
         
     # list of cluster numbers
+    if VERBOSE:
+        print("Saving list of retained cluster numbers")
     filtered_cluster_numbers = [cluster['number'] 
                                 for organism in filtered_session_dict['organisms'] 
                                 for scaffold in organism['scaffolds'] 
@@ -606,6 +689,7 @@ def main():
     global ASSIGNED_TEMP_DIR
     global GENOMES
     global SKDER_OUT
+    global VERBOSE
     
     ## Parse arguments
     args = parse_arguments()
@@ -614,6 +698,7 @@ def main():
     temp_dir = args.temp_dir.rstrip('/')
     path_to_session = os.path.join(os.getcwd(), args.session_file)
     nb_cores = int(args.cores)
+    VERBOSE = args.verbose
     ani_threshold = float(args.ani)
     no_recovery_by_content = args.no_recovery_by_content
     no_recovery_by_score = args.no_recovery_by_score
@@ -657,8 +742,12 @@ def main():
         
         ## Finish by copying over intermediate results if flagged, and removing the temporary folder
         if keep_intermediate or keep_downloads:
+            if VERBOSE:
+                print('Copying downloaded genomes to output folder')
             shutil.copytree(GENOMES, os.path.join('data', 'genomes'), dirs_exist_ok = True)
         if keep_intermediate or keep_dereplication:
+            if VERBOSE:
+                print("Copying skDER results to output folder")
             shutil.copytree(SKDER_OUT, os.path.join('data', 'skder_out'), dirs_exist_ok = True)
             
     print(f"\nAll done! Results can be found in {work_dir}")
