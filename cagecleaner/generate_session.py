877ecb823fc255f1370a993f7e54ce3b01596efc
Lucas De Vrieze <lucas.devrieze@kuleuven.be>
2026-04-21 15:46:33 +0200

    Minor bugfix

--- cagecleaner/generate_session.py
+++ cagecleaner/generate_session.py
@@ -111,7 +111,7 @@ def main():
     args = parse_arguments()
     
     # Validate arguments
-    validate_arguments()
+    validate_arguments(args)
     
     # Generate session
     LOG.info("Generating cblaster session.")
